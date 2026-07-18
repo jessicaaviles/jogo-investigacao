ca4bdedf9741c66b7346b57ec4bd904cd1e7f5f3
Jessica Aviles Barbalho <jessica.aviles16@gmail.com>
2026-07-17 23:24:11 -0300

    bump service worker cache version para limpar cache

--- frontend/public/sw.js
+++ frontend/public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'ultimo-vestigio-static-v2';
+const CACHE = 'ultimo-vestigio-static-v3';
 self.addEventListener('install', (event) => {
   event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(['/','/manifest.webmanifest'])));
   self.skipWaiting();
